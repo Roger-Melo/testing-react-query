72ac1a3f2e0730038fd782bda115404217bb89bd
Roger-Melo <rogerw592@gmail.com>
2024-03-21 16:59:33 -0300

    ✅ fetch user data example completed

--- src/app.jsx
+++ src/app.jsx
@@ -1,3 +1,49 @@
-const App = () => <h1>Boilerplate Vite + React</h1>
+import { useQuery } from '@tanstack/react-query'
+
+const fetchUser = username => fetch(`https://api.github.com/users/${username}`)
+  .then(res => res.json())
+
+const GitHubUser = ({ username }) => {
+  const { isError, error, isLoading, data } = useQuery({ queryKey: [username], queryFn: () => fetchUser(username), refetchOnWindowFocus: false })
+  return isError
+    ? <p>{error.message}</p>
+    : isLoading
+      ? <p>Carregando informações...</p>
+      : (
+        <ul>
+          {Object.entries(data).map(([key, value]) =>
+            <li key={key}>
+              <b>{key}</b>: {value}
+            </li>)
+          }
+        </ul>
+      )
+}
+
+const App = () => <GitHubUser username="roger-melo" />
 
 export { App }
+/*
+const RandomNumber = () => {
+  const { isError, error, isFetching, isLoading, data, refetch } =
+    useQuery({ queryKey: ['randomNumber'], queryFn: fetchRandomNumber })
+
+  return isError
+    ? <p>{error.message}</p>
+    : (
+      <>
+        <h2>Número aleatório: {isFetching || isLoading ? '(carregando...)' : data}</h2>
+        <button onClick={refetch} disabled={isFetching || isLoading}>Gerar número</button>
+      </>
+    )
+}
+
+const App = () =>
+  <>
+    <QueryClientProvider client={queryClient}>
+      <RandomNumber />
+    </QueryClientProvider>
+  </>
+
+export { App }
+*/
