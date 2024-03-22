32e97737509bdd95f554f7005f91b45dbceda188
Roger-Melo <rogerw592@gmail.com>
2024-03-22 16:29:49 -0300

    🚧 Add IssuesList

--- src/app.jsx
+++ src/app.jsx
@@ -1,17 +1,22 @@
 import { useQuery } from '@tanstack/react-query'
 
+const fetchLabels = ({ organization, repository }) =>
+  fetch(`https://api.github.com/repos/${organization}/${repository}/labels`)
+    .then(res => res.json())
+    .then(data => data.map(({ id, name, color }) => ({ id, name, color })))
+
 const fetchIssues = ({ organization, repository }) =>
   fetch(`https://api.github.com/repos/${organization}/${repository}/issues`)
     .then(res => res.json())
     .then(data => {
-      return data.map(issue => ({
-        id: issue.id,
-        state: issue.state,
-        title: issue.title,
-        createdAt: issue.created_at,
-        author: { name: issue.user.login, avatar: issue.user.avatar_url },
-        labels: issue.labels.map(label => ({ id: label.id, color: label.color, name: label.name })),
-        url: issue.html_url
+      return data.map(({ id, state, title, created_at, user, labels, html_url }) => ({
+        id,
+        state,
+        title,
+        createdAt: created_at,
+        author: { name: user.login, avatar: user.avatar_url },
+        labels: labels.map(label => ({ id: label.id, color: label.color, name: label.name })),
+        url: html_url
       }))
     })
 
@@ -32,7 +37,7 @@ const IssueItem = ({ state, title, createdAt, labels, author, url }) =>
     </div>
     {labels.length > 0 && (
       <p className="labels">Labels: {labels.map(({ id, color, name }) =>
-        <span key={id} style={{ backgroundColor: `#${color}` }}>{name}</span>)}
+        <span key={id} style={{ backgroundColor: `#${color}` }} className="label">{name}</span>)}
       </p>
     )}
   </li>
@@ -51,10 +56,35 @@ const IssuesList = () => {
       : <ul className="issuesList">{data.map(issue => <IssueItem key={issue.id} {...issue} />)}</ul>
 }
 
+const LabelItem = ({ name, color }) =>
+  <li>
+    <span style={{ backgroundColor: `#${color}` }} className="label">{name}</span>
+  </li>
+
+const LabelsList = () => {
+  const { isError, error, isLoading, data } = useQuery({
+    queryKey: ['labels'],
+    queryFn: () => fetchLabels({ organization: 'frontendbr', repository: 'vagas' }),
+    refetchOnWindowFocus: false
+  })
+
+  return isError
+    ? <p>{error.message}</p>
+    : isLoading
+      ? <p>Carregando informações...</p>
+      : <ul className="labelsList">{data.map(label => <LabelItem key={label.id} {...label} />)}</ul>
+}
+
 const App = () =>
-  <>
-    <h1>Vagas</h1>
-    <IssuesList />
-  </>
+  <div className="app">
+    <div>
+      <h1>Vagas</h1>
+      <IssuesList />
+    </div>
+    <div>
+      <h2>Labels</h2>
+      <LabelsList />
+    </div>
+  </div>
 
 export { App }
