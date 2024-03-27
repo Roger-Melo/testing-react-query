9a7cf7b7ac47961bd2a43e268cfb54ede64d1e3d
Roger-Melo <rogerw592@gmail.com>
2024-03-27 09:57:33 -0300

    ✅♻  Start code for lesson 66

--- src/app.jsx
+++ src/app.jsx
@@ -1,119 +1,61 @@
 import { useQuery } from '@tanstack/react-query'
-import { useState } from 'react'
 
-const fetchIssues = activeLabels => {
-  const labelsParam = activeLabels.length === 0
-    ? ''
-    : `?labels=${activeLabels.map(label => label.name).join(',')}`
-  return fetch(`https://api.github.com/repos/frontendbr/vagas/issues${labelsParam}`)
+const fetchIssues = ({ organization, repository }) =>
+  fetch(`https://api.github.com/repos/${organization}/${repository}/issues`)
     .then(res => res.json())
-    .then(data => data.map(issue => ({
-      id: issue.id,
-      state: issue.state,
-      title: issue.title,
-      createdAt: issue.created_at,
-      author: { username: issue.user.login, avatar: issue.user.avatar_url },
-      labels: issue.labels.map(label => ({ id: label.id, color: label.color, name: label.name })),
-      url: issue.html_url
-    })))
-}
-
-const fetchLabels = () =>
-  fetch('https://api.github.com/repos/frontendbr/vagas/labels?per_page=100')
-    .then(res => res.json())
-    .then(data => data.map(label => ({ id: label.id, name: label.name, color: label.color })))
+    .then(data => {
+      return data.map(issue => ({
+        id: issue.id,
+        state: issue.state,
+        title: issue.title,
+        createdAt: issue.created_at,
+        author: { name: issue.user.login, avatar: issue.user.avatar_url },
+        labels: issue.labels.map(label => ({ id: label.id, color: label.color, name: label.name })),
+        url: issue.html_url
+      }))
+    })
 
 const getFormattedDate = date => {
   const [year, month, day] = date.split('T')[0].split('-')
   return `${day}/${month}/${year}`
 }
 
-const Label = ({ isActive = false, label, onClickLabel }) =>
-  <button
-    onClick={() => onClickLabel(label)}
-    className={`label ${isActive ? 'activeLabel' : ''}`}
-    style={{ backgroundColor: `#${label.color}` }}
-  >
-    {label.name}
-  </button>
-
-const IssueItem = ({ state, title, createdAt, labels, author, url, onClickLabel }) =>
+const IssueItem = ({ state, title, createdAt, labels, author, url }) =>
   <li>
     <span>{state}</span>
     <h3>
       <a href={url} target="_blank" rel="noreferrer">{title}</a>
     </h3>
     <div className="createdBy">
-      <p>Criada em {getFormattedDate(createdAt)}, por {author.username}</p>
-      <img src={author.avatar} alt={`Foto de ${author.username}`} />
+      <p>Criada em {getFormattedDate(createdAt)}, por {author.name}</p>
+      <img src={author.avatar} alt={`Foto de ${author.name}`} />
     </div>
     {labels.length > 0 && (
-      <p>Labels: {labels.map(label =>
-        <Label key={label.id} onClickLabel={onClickLabel} label={label} />)}
+      <p className="labels">Labels: {labels.map(({ id, color, name }) =>
+        <span key={id} style={{ backgroundColor: `#${color}` }}>{name}</span>)}
       </p>
     )}
   </li>
 
-const IssuesList = ({ activeLabels, onClickLabel }) => {
-  const { isError, isLoading, isSuccess, error, data } = useQuery({
-    queryKey: ['issues', { activeLabels: activeLabels.map(({ name }) => name) }, activeLabels],
-    queryFn: () => fetchIssues(activeLabels),
-    refetchOnWindowFocus: false,
-    staleTime: Infinity
+const IssuesList = () => {
+  const { isError, error, isLoading, data } = useQuery({
+    queryKey: ['issues'],
+    queryFn: () => fetchIssues({ organization: 'frontendbr', repository: 'vagas' }),
+    refetchOnWindowFocus: false
   })
 
-  return (
-    <div className="issuesListContainer">
-      <h1>Vagas</h1>
-      {isError && <p>{error.message}</p>}
-      {isLoading && <p>Carregando informações...</p>}
-      {isSuccess && (
-        <ul className="issuesList">
-          {data.map(issue => <IssueItem key={issue.id} onClickLabel={onClickLabel} {...issue} />)}
-        </ul>
-      )}
-    </div>
-  )
+  return isError
+    ? <p>{error.message}</p>
+    : isLoading
+      ? <p>Carregando informações...</p>
+      : (
+        <>
+          <h1>Vagas</h1>
+          <ul className="issuesList">{data.map(issue => <IssueItem key={issue.id} {...issue} />)}</ul>
+        </>
+      )
 }
 
-const LabelsList = ({ activeLabels, onClickLabel }) => {
-  const { isError, isLoading, isSuccess, error, data } = useQuery({
-    queryKey: ['labels'],
-    queryFn: fetchLabels,
-    refetchOnWindowFocus: false,
-    staleTime: Infinity
-  })
-
-  return (
-    <div className="labelsListContainer">
-      <h2>Labels</h2>
-      {isError && <p>{error.message}</p>}
-      {isLoading && <p>Carregando informações...</p>}
-      {isSuccess && (
-        <ul className="labelsList">
-          {data.map(label => {
-            const isActive = activeLabels.some(activeLabel => label.id === activeLabel.id)
-            return <Label key={label.id} isActive={isActive} label={label} activeLabels={activeLabels} onClickLabel={onClickLabel} />
-          })}
-        </ul>
-      )}
-    </div>
-  )
-}
-
-const App = () => {
-  const [activeLabels, setActiveLabels] = useState([])
-  const markAsActive = label => setActiveLabels(prev => {
-    const isAlreadyActive = prev.some(prevLabel => prevLabel.id === label.id)
-    return isAlreadyActive ? prev.filter(prevLabel => prevLabel.id !== label.id) : [...prev, label]
-  })
-
-  return (
-    <div className="app">
-      <IssuesList activeLabels={activeLabels} onClickLabel={markAsActive} />
-      <LabelsList activeLabels={activeLabels} onClickLabel={markAsActive} />
-    </div>
-  )
-}
+const App = () => <IssuesList />
 
 export { App }
