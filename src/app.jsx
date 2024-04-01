7e3c11a3472c8ffd645021183d15861b1bd8c0a1
Roger-Melo <rogerw592@gmail.com>
2024-04-01 13:15:58 -0300

    ✅ Add button to clear issues search

--- src/app.jsx
+++ src/app.jsx
@@ -70,19 +70,22 @@ const IssueItem = ({ state, title, createdAt, labels, author, url, onClickLabel 
     )}
   </li>
 
-const SearchIssues = ({ formRef, onSearchIssues }) =>
-  <form ref={formRef} onSubmit={onSearchIssues}>
-    <input
-      type="search"
-      name="inputSearchIssues"
-      className="inputSearchIssues"
-      placeholder="React"
-      minLength={2}
-      required
-      autoFocus
-    />
-    <button>Pesquisar</button>
-  </form>
+const SearchIssues = ({ formRef, searchedIssuesQuery, onSearchIssues, onClearSearchedIssues }) =>
+  <div className="searchIssues">
+    <form ref={formRef} onSubmit={onSearchIssues}>
+      <input
+        type="search"
+        name="inputSearchIssues"
+        className="inputSearchIssues"
+        placeholder="React"
+        minLength={2}
+        required
+        autoFocus
+      />
+      <button>Pesquisar</button>
+    </form>
+    {searchedIssuesQuery.data && <button onClick={onClearSearchedIssues}>Limpar Pesquisa</button>}
+  </div>
 
 const IssuesList = ({ activeLabels, onClickLabel }) => {
   const [searchTerm, setSearchTerm] = useState('')
@@ -117,6 +120,8 @@ const IssuesList = ({ activeLabels, onClickLabel }) => {
     setSearchTerm(inputSearchIssues.value)
   }
 
+  const clearSearchedIssues = () => setSearchTerm('')
+
   const isLoading = issuesQuery.isLoading || searchedIssuesQuery.isLoading
   const isError = issuesQuery.isError || searchedIssuesQuery.isError
   const errorMessage = issuesQuery.error?.message || searchedIssuesQuery.error?.message
@@ -125,7 +130,12 @@ const IssuesList = ({ activeLabels, onClickLabel }) => {
   return (
     <div className="issuesListContainer">
       <h1>Vagas</h1>
-      <SearchIssues onSearchIssues={searchIssues} formRef={formRef} />
+      <SearchIssues
+        onSearchIssues={searchIssues}
+        formRef={formRef}
+        searchedIssuesQuery={searchedIssuesQuery}
+        onClearSearchedIssues={clearSearchedIssues}
+      />
       {isError && <p>{errorMessage}</p>}
       {isLoading && <p>Carregando informações...</p>}
       <ul className="issuesList">
