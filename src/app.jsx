34914203535a2050176022ae77b63bf9ee2d4aa5
Roger-Melo <rogerw592@gmail.com>
2024-04-01 14:29:15 -0300

    ✅ Add total count on searched issues title

--- src/app.jsx
+++ src/app.jsx
@@ -23,15 +23,18 @@ const fetchSearchedIssues = searchTerm => {
     encodeURIComponent(`${searchTerm} repo:frontendbr/vagas is:issue is:open`)
   return fetch(`https://api.github.com/search/issues${queryString}`)
     .then(res => res.json())
-    .then(data => data.items.map(issue => ({
-      id: issue.id,
-      state: issue.state,
-      title: issue.title,
-      createdAt: issue.created_at,
-      author: { username: issue.user.login, avatar: issue.user.avatar_url },
-      labels: issue.labels.map(label => ({ id: label.id, color: label.color, name: label.name })),
-      url: issue.html_url
-    })))
+    .then(data => ({
+      totalCount: data.total_count,
+      issues: data.items.map(issue => ({
+        id: issue.id,
+        state: issue.state,
+        title: issue.title,
+        createdAt: issue.created_at,
+        author: { username: issue.user.login, avatar: issue.user.avatar_url },
+        labels: issue.labels.map(label => ({ id: label.id, color: label.color, name: label.name })),
+        url: issue.html_url
+      }))
+    }))
 }
 
 const fetchLabels = () =>
@@ -126,11 +129,14 @@ const IssuesList = ({ activeLabels, onClickLabel }) => {
   const isLoading = issuesQuery.isLoading || searchedIssuesQuery.isLoading
   const isError = issuesQuery.isError || searchedIssuesQuery.isError
   const errorMessage = issuesQuery.error?.message || searchedIssuesQuery.error?.message
-  const queryToBeDisplayed = searchedIssuesQuery.isSuccess ? searchedIssuesQuery : issuesQuery
+  const titleMessage = `com o termo "${searchTerm}": ${searchedIssuesQuery.data?.totalCount}`
+  const dataToRender = searchedIssuesQuery.isSuccess
+    ? searchedIssuesQuery.data.issues
+    : issuesQuery.data
 
   return (
     <div className="issuesListContainer">
-      <h1>Vagas</h1>
+      <h1>Vagas {searchedIssuesQuery.isSuccess && titleMessage}</h1>
       <SearchIssues
         onSearchIssues={searchIssues}
         formRef={formRef}
@@ -140,7 +146,7 @@ const IssuesList = ({ activeLabels, onClickLabel }) => {
       {isError && <p>{errorMessage}</p>}
       {isLoading && <p>Carregando informações...</p>}
       <ul className="issuesList">
-        {queryToBeDisplayed.data?.map(issue =>
+        {dataToRender?.map(issue =>
           <IssueItem key={issue.id} onClickLabel={onClickLabel} {...issue} />)}
       </ul>
     </div>
